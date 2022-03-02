34d69867ea019b90861010a81abf1ba4be9441eb
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-02 11:18:18 -0500

    set up the routes to the server in server.js

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs =  require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', exphbs.engine());
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {maxAge: 1000*60*60}, //this is to log you out after 60 mins of inactivity
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine());
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 //Setup routes to the Server
 //Look at /controllers folder
-app.use('/', routes);
+
+app.use('./controllers', routes);
 
 sequelize.sync({force:true}).then(()=>{
     app.listen(PORT, () => {
