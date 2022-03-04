d741f27bb86c0b4ade4da1c53a5b400705195270
Jen Truong <jennifertruong09@gmail.com>
2022-03-04 14:53:38 -0500

    got rid of title from the public js files

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,13 +1,13 @@
 const newPostFormHandler = async (event) => {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value.trim();
+
     const post = document.querySelector('textarea[name="post-body"]').value.trim();
     
     await fetch('/api/post', {
         method: 'POST',
         body: JSON.stringify({
-          title,
+
           post,
         }),
         headers: { 'Content-Type': 'application/json' },
--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,13 +1,12 @@
 const editFormHandler = async function(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value;
+  
     const post = document.querySelector('textarea[name="post-body"]').value;
   
     await fetch(`/api/post/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title,
         post
       }),
       headers: {
