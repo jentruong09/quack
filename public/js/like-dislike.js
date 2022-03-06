0c5d2c493e5c82a7a28e828c6cb75a1b710d60c5
Vy Nguyen <vy1872@gmail.com>
2022-03-06 14:11:02 -0600

    still working on it

---
+++ public/js/like-dislike.js
@@ -0,0 +1,9 @@
+// const emoji = require('node-emoji');
+const likes = document.querySelectorAll(`#likes`);
+const dislike = document.querySelectorAll(`#dislikes`);
+
+Array.prototype.forEach.call(likes, dislike, (like, dislike) =>{
+    like.textContent("why isn't this working")
+    dislike.textContent("why isn't this working")
+})
+
